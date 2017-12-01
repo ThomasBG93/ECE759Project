d55befa43f3f8393594711b023b439890fd8c48f
Ubuntu <brian@guttag.com>
2017-12-01 14:17:14 -0600

    added openmp support for movement

--- movement.c
+++ movement.c
@@ -4,14 +4,20 @@
 #include <omp.h>
 #include "creature_char.h"
 
+
+void move(int i, int len, creature *array);
+
 int main(int argc, char *argv[])
 {
 	if(argc < 3){
-		printf("Wrong input, should be: ./move len numCreatures\n");
+		printf("Wrong input, should be: ./move length numCreatures numThreads\n");
 		return 1;
 	}
 	int size = atoi(argv[2]); //numCreatures;
 	int len = atoi(argv[1]);	//length of one side
+	int numThreads = atoi(argv[3]);
+	//TODO should numThreads be set globally?
+	omp_set_num_threads(numThreads);
 	int i = 0;
 	FILE* fp;
 	creature *array = (creature *)malloc(size * sizeof(creature));
@@ -28,29 +34,22 @@ int main(int argc, char *argv[])
 	}
 	fclose(fp);
 	srand(time(NULL));
-	int xchange = 0;
-	int ychange = 0;
-	for(i=0; i < size; i++){
-		xchange = (rand()%3) -1;
-		ychange = (rand()%3) -1;
-		//printf("Before[%d]: (%d,%d)\n",i,array[i].xPos,array[i].yPos);
-		//TODO do we want to wrap or fall off?
-		array[i].xPos = array[i].xPos + xchange;
-		if(array[i].xPos >= len){
-			array[i].xPos = 0;
+	double start, end, time;
+	start = omp_get_wtime();
+	#pragma omp parallel
+	{
+		#pragma omp for
+		for(i=0; i < size; i++){
+			//printf("Before[%d]: (%d,%d)\n",i,array[i].xPos,array[i].yPos);
+			move(i,len,array);
+			//printf("After[%d]: (%d,%d)\n",i,array[i].xPos,array[i].yPos);
 		}
-		if(array[i].xPos < 0){
-			array[i].xPos = len - 1;
-		}
-		array[i].yPos = array[i].yPos + ychange;
-		if(array[i].yPos >= len){
-			array[i].yPos = 0;
-		}
-		if(array[i].yPos < 0){
-			array[i].yPos = len - 1;
-		}
-		//printf("After[%d]: (%d,%d)\n",i,array[i].xPos,array[i].yPos);
+
 	}
+	end = omp_get_wtime();
+	time = (end-start)*1000;
+	printf("Time: %f\n",time);
+	
 
 	fp = fopen("testMove.txt", "w");
 	for(int i =0; i < size; i++){
@@ -69,4 +68,27 @@ int main(int argc, char *argv[])
 	free(array);
 	return 0;
 
+}
+
+void move(int i, int len, creature *array)
+{
+	int xchange = (rand()%3) -1;
+	int ychange = (rand()%3) -1;
+	
+	//TODO do we want to wrap or fall off?
+	array[i].xPos = array[i].xPos + xchange;
+	if(array[i].xPos >= len){
+		array[i].xPos = 0;
+	}
+	if(array[i].xPos < 0){
+		array[i].xPos = len - 1;
+	}
+	array[i].yPos = array[i].yPos + ychange;
+	if(array[i].yPos >= len){
+		array[i].yPos = 0;
+	}
+	if(array[i].yPos < 0){
+		array[i].yPos = len - 1;
+	}
+	
 }
